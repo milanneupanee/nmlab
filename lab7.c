1ee026cfd092d231a1733013d14ddb44d4caf5c2
Milan Neupane <117293783+milanneupanee@users.noreply.github.com>
2024-01-23 20:44:56 +0545

    jacobis

--- lab7.c
+++ lab7.c
@@ -1,16 +1,39 @@
-//lab implemantation of gauss jorden method
-#include<stdio.h>
+//Implemanatation of Gauss Jacobis method using c
+ #include<stdio.h>
+ #include<conio.h>
+ #include<math.h>
+float  fx(float x,float y ,float z){
+	x=(17-y+2*z)/20;
+	return x; 
+	
+}
+float  fy(float x,float y, float z){
+	y=(-18-3*x+z)/20;
+	return y; 
+	
+}
+float   fz(float x,float y, float z){
+	z=(25+3*y-2*x)/20;
+	return z; 
+	
+}
 int main(){
-	float a[100][100];
-	int n;
-	float a[100];
-	printf("Enter the no of unknown variables");
-	scanf("%d",&n);
-	printf("Enter the element of the agumented matrix");
-	for( int i =1;i<n+1;i++){
-		for( int j =1;j<n+1;j++){
-			scanf("%f",&a[i][j]);
-			}
-		}
+	float e=0.001;
+	float x=0,y=0,z=0;
+	float ex,ey,ez;
+
+	do
+	{
+	ex=fabs(x-fx(x,y,z));
+	ey=fabs(y-fy(x,y,z));
+	ez=fabs(z-fz(x,y,z));
+	x=fx(x,y,z);
+	y=fy(x,y,z);
+	z=fz(x,y,z);
+	}
+		while (ex>e&&ey>e&&ez>e);
+
+	printf("The value of x=%f\n y=%f\n z=%f\n",fx(x,y,z),fy(x,y,z),fz(x,y,z));
+	return 0;
 	
 }
